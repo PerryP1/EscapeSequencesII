685136ef9cf91dd15a5cf78ef9395aa3ca495fd5
PerryP1 <perryapublico@gmail.com>
2018-07-11 17:47:27 -0400

    Making updates...

---
+++ src/EscapeSequencesII.java
@@ -0,0 +1,25 @@
+public class EscapeSequencesII {
+
+    public static void main (String[] args) {
+
+
+    // Write a Java program to print the following columns of values. Use \t for the tabs between the columns.
+    //Cost      Quantity   Total
+    //$1,000.00   4       $4,000.00
+    //$   50.00   8         $400.00
+    //______________________________
+    //TOTAL:      12      $4,400.00
+    //*
+
+    /*
+
+
+
+     */
+        System.out.println("Cost      Quantity   Total");
+        System.out.println("$1,000.00   4       $4,000.00");
+        System.out.println("$   50.00   8         $400.00");
+        System.out.println("_____________________________");
+        System.out.println("TOTAL:      12      $4,400.00");
+
+    }}
